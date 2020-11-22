11f3cf0af9b7204a4eaa04a7cae44ec5c4f0e0ce
Nkundibiza Ally <nkundibizaally@gmail.com>
2020-11-22 17:14:29 +0100

    Simplified the Timer0 code in normal mode

--- AVR_C_Timer0/main.c
+++ AVR_C_Timer0/main.c
@@ -4,15 +4,19 @@
  * Created: 11/22/2020 2:20:06 AM
  * Author : NKUNDIBIZA ALLY
  */ 
+#define OC0		0x03
+
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 
 int main(void)
 {
-    
-    while (1) 
-    {
-    }
+
+	DDRB = (1 << OC0);	// Set 0C0 pin as OUTPUT
+    TCCR0 =  (1 << COM00) | (5 << CS00);  // Normal mode toggle OC0 pin on MAX 
+
 }
+
 
