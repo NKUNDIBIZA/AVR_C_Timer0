f26c8136f31a914149b24d3e4738b4124bc94625
Nkundibiza Ally <nkundibizaally@gmail.com>
2020-11-22 02:23:23 +0100

    Inital commit

---
+++ AVR_C_Timer0/main.c
@@ -0,0 +1,18 @@
+/*
+ * AVR_C_Timer0.c
+ *
+ * Created: 11/22/2020 2:20:06 AM
+ * Author : NKUNDIBIZA ALLY
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    
+    while (1) 
+    {
+    }
+}
+
